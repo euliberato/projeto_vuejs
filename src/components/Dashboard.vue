f0cf0b078fc60a82da5f95ddc20a5ae24dc08bca
euliberato <ruanlib.2003@gmail.com>
2022-11-30 14:05:26 -0300

    Criando template para Dashboard da view 'Pedidos'

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,99 @@
+<template>
+    <div id="burguer-table">
+        <div>
+            <div id="burguer-table-heading">
+                <div class="order-id">#:</div>
+                <div>Cliente: </div>
+                <div>Pão: </div>
+                <div>Carne: </div>
+                <div>Opcionais: </div>
+                <div>Ações: </div>
+            </div>
+        </div>
+        <div id="burguer-table-rows">
+            <div class="burguer-table-row">
+                <div class="order-number">1</div>
+                <div>João</div>
+                <div>Info 1</div>
+                <div>Info 2</div>
+                <div>
+                    <ul>
+                        <li>Opções 1</li>
+                        <li>Opções 2</li>
+                    </ul>
+                </div>
+                <div>
+                    <select name="status" class="status">
+                        <option value="">Selecione:</option>
+                    </select>
+                    <button class="delete-btn">Cancelar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Dashboard"
+}
+</script>
+
+<style scoped>
+    #burguer-table {
+        max-width: 1200px;
+        margin: 0 auto;
+
+    }
+
+    #burguer-table-heading,
+    #burguer-table-rows,
+    .burguer-table-row {
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+    #burguer-table-heading {
+        font-weight: bold;
+        padding: 12px;
+        border-bottom: 3px solid #333;
+
+    }
+
+    #burguer-table-heading div,
+    .burguer-table-row div {
+        width: 19%;
+    }
+
+    .burguer-table-row {
+        width: 100%;
+        padding: 12px;
+        border-bottom: 1px solid #ccc;
+    }
+
+    #burguer-table-heading .order-id, 
+    .burguer-table-row .order-number {
+        width: 5%;
+    }
+
+    select {
+        padding: 12px 6px;
+        margin-right: 12px;
+    }
+
+    .delete-btn {
+        background-color: #222;
+        color: #fcba30;
+        font-weight: bold;
+        border: 2px solid #222;
+        padding: 10px;
+        margin: 0 auto;
+        cursor: pointer;
+        transition: .5s;
+    }
+
+    .delete-btn:hover {
+        background: transparent;
+        color: #222;
+    }
+</style>
